6f8b0718ccdec5864e3ada5ff71be0c4ffd58cc5
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-26 15:47:53 -0700

    created popup js and close function. added 2nd API for policy. added comments in js file

--- js/app.js
+++ js/app.js
@@ -1,26 +1,48 @@
-$('#modal').hide();
-
 $(function (){
+  $('.popUp').hide();
+  $('.popUpClose').on('click', function(){
+    $('.popUp').hide()
+  });
+
   $('#map').usmap({
     'stateStyles': {fill: 'rgb(74, 137, 163)'},
     'stateHoverStyles': {fill: 'rgb(171, 73, 73)'},
     'stateHoverAnimation': 300,
     click: function(event, data) {
+      // persistence function
+      // if(localStorage.getItem(data.name)){
+      //   var temp = localStorage.getItem(data.name);
+      //   temp.count++;
+      // }else{
+      //   localStorage.setItem({state: data.name, count: 1});
+      // }
+
+      // pop-up function
+      (function pop (){
+        $('.popUp').show()
+      })()
+
+      // solar count & cost API
       $.ajax({
         url: 'https://developer.nrel.gov/api/solar/open_pv/installs/rankings?state='+data.name+'&api_key=baBQnhHJIUy28EX60XZ2mpnTHSQ4OkRuRE6Ki4yS&format=JSON',
         type: 'GET',
         dataType: 'json',
         success: function(response){
           console.log(response)
-          $('#name').text = response.state;
-          $('#modal').show();
         }
-      })
+      });
+
+      // available policies API
+      $.ajax({
+        url: 'https://developer.nrel.gov/api/energy_incentives/v2/dsire.json?api_key=baBQnhHJIUy28EX60XZ2mpnTHSQ4OkRuRE6Ki4yS&address='+data.name+'&technology=solar_photovoltaics',
+        type: 'GET',
+        dataType: 'json',
+        success: function(response){
+          console.log(response)
+        }
+      });
+
     }
   })
-})
 
-
-$('#close').on('click', function(){
-  $('#modal').hide();
 })
