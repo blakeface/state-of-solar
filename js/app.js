558ba0c5fa12e03c2a16148e8886b29e18fe52b4
Blake Netzeband <blake.netzeband@gmail.com>
2016-03-03 15:16:45 -0700

    fixed min height bug on popup. erased mobile view.

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ $(function (){
   keyCap,
   keyCount,
   keyCost,
-  width = $(window).width(),
   states = [
     ['Arizona', 'AZ'],
     ['Alabama', 'AL'],
@@ -178,159 +177,82 @@ $(function (){
     $('#cost').append("<h3>Average National Costs:</h3><h4>$" + (costTotal/costArr.length).toFixed(2) + " / watt</h4>");
     $('#cap').append("<h3>Total Capacity:</h3><h4>" + capTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " kW</h4>");
 
-    // mobile view
-    if ( width < 320 ) {
-      $('.mapLarge').hide();
-      $('#mapSmall').usmap({
-        'stateStyles': {fill: '#'},
-        'stateHoverStyles': {fill: 'rgb(70, 112, 161)'},
-        'stateHoverAnimation': 150,
-        'stroke': {fill: '#ffffff'},
-        click: function(event, data) {
-          clickVerifier++;
-          console.log(clickVerifier);
-
-          // state abbreviation translator
-          (function abbrState(abbr){
-            for (var j = 0; j < states.length; j++) {
-              if (states[j][1] == data.name) stateName = (states[j][0])
-            };
-          })()
-
-          // sorting function
-          function costRank () {
-            for (var i = 0; i < costArr.length; i++) {
-              if ( costArr[i][0] == data.name ) costRanking = i + 1;
-            }
-          }
-          function capRank () {
-            for (var i = 0; i < capArr.length; i++) {
-              if ( capArr[i][0] == data.name ) capRanking = i + 1;
-            }
-          }
-          function countRank () {
-            for (var i = 0; i < countArr.length; i++) {
-              if ( countArr[i][0] == data.name ) countRanking = i + 1;
-            }
-          }
-
-          // persistence function
-          if ( localStorage.getItem(stateName) !== 'null'){
-            clickCount = +(localStorage.getItem(stateName)) + 1;
-            localStorage.setItem(stateName, clickCount)
-          } else localStorage.setItem(stateName, clickCounter);
-
-          // pop-out function
-          $.ajax({
-            url: 'https://developer.nrel.gov/api/energy_incentives/v2/dsire.json?api_key=zRvnoStLNlMEuI4UIT0hyYzDa5j3p83JKfaVTbKs&address='+data.name+'&technology=solar_photovoltaics',
-            dataType: 'JSON',
-            type: 'GET',
-            success: function(response){
-              stateData[data.name].policies = response.result.length;
-            },
-          }).then(function(){
-            countRank(data.name);
-            capRank(data.name);
-            costRank(data.name);
-            for (var key in stateData) {
-              keyCount = stateData[key].count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-              keyCap = stateData[key].cap.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-              keyCost = stateData[key].cost.toFixed(2);
-
-              if ( clickVerifier >! 1 ) {
-                if(data.name == key){
-                  (function pop (){
-                    $('.pu').show();
-                    $('.puTitleHolder').append("<div class='puTitle'><h3>" + stateName + "</h3></div>");
-                    $('.puTable').append("<tr class='rowRemove'><td class='rowRemove'>" + keyCount + " Systems Installed</td><td class='puCenter rowRemove'>" + countRanking + "</td></tr><tr class='rowRemove'><td class='rowRemove'>" + keyCap + " kW Capacity</td><td class='puCenter rowRemove'>" + capRanking + "</td></tr class='rowRemove'><tr><td class='rowRemove'>Avg. Cost: $" + keyCost + " / watt</td><td class='puCenter rowRemove'>" + costRanking + "</td></tr>");
-                    if (typeof stateData[key].policies == 'number') {
-                      $('.puTable').append("<tr class='rowRemove'><td class='rowRemove' colspan=2>" + stateData[key].policies + " Solar Policies Enacted</td></tr>")
-                    }
-                  }())
-                };
-              }
-            }
-          })
-        }
-      })
-    }
-
     // notebook view
-    if ( width > 320 ) {
-      $('.mapSmall').hide();
-      $('#mapLarge').usmap({
-        'stateStyles': {fill: '#'},
-        'stateHoverStyles': {fill: 'rgb(70, 112, 161)'},
-        'stateHoverAnimation': 150,
-        'stroke': {fill: '#ffffff'},
-        click: function(event, data) {
-          clickVerifier++;
-          console.log(clickVerifier);
+    $('.mapSmall').hide();
+    $('#mapLarge').usmap({
+      'stateStyles': {fill: '#'},
+      'stateHoverStyles': {fill: 'rgb(70, 112, 161)'},
+      'stateHoverAnimation': 150,
+      'stroke': {fill: '#ffffff'},
+      click: function(event, data) {
+        clickVerifier++;
+        console.log(clickVerifier);
 
-          // state abbreviation translator
-          (function abbrState(abbr){
-            for (var j = 0; j < states.length; j++) {
-              if (states[j][1] == data.name) stateName = (states[j][0])
-            };
-          })()
+        // state abbreviation translator
+        (function abbrState(abbr){
+          for (var j = 0; j < states.length; j++) {
+            if (states[j][1] == data.name) stateName = (states[j][0])
+          };
+        })()
 
-          // sorting function
-          function costRank () {
-            for (var i = 0; i < costArr.length; i++) {
-              if ( costArr[i][0] == data.name ) costRanking = i + 1;
-            }
+        // sorting function
+        function costRank () {
+          for (var i = 0; i < costArr.length; i++) {
+            if ( costArr[i][0] == data.name ) costRanking = i + 1;
           }
-          function capRank () {
-            for (var i = 0; i < capArr.length; i++) {
-              if ( capArr[i][0] == data.name ) capRanking = i + 1;
-            }
-          }
-          function countRank () {
-            for (var i = 0; i < countArr.length; i++) {
-              if ( countArr[i][0] == data.name ) countRanking = i + 1;
-            }
-          }
-
-          // persistence function
-          if ( localStorage.getItem(stateName) !== 'null'){
-            clickCount = +(localStorage.getItem(stateName)) + 1;
-            localStorage.setItem(stateName, clickCount)
-          } else localStorage.setItem(stateName, clickCounter);
-
-          // pop-out function
-          $.ajax({
-            url: 'https://developer.nrel.gov/api/energy_incentives/v2/dsire.json?api_key=zRvnoStLNlMEuI4UIT0hyYzDa5j3p83JKfaVTbKs&address='+data.name+'&technology=solar_photovoltaics',
-            dataType: 'JSON',
-            type: 'GET',
-            success: function(response){
-              stateData[data.name].policies = response.result.length;
-            },
-          }).then(function(){
-            countRank(data.name);
-            capRank(data.name);
-            costRank(data.name);
-            for (var key in stateData) {
-              keyCount = stateData[key].count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-              keyCap = stateData[key].cap.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-              keyCost = stateData[key].cost.toFixed(2);
-
-              if ( clickVerifier >! 1 ) {
-                if(data.name == key){
-                  (function pop (){
-                    $('.pu').show();
-                    $('.puTitleHolder').append("<div class='puTitle'><h3>" + stateName + "</h3></div>");
-                    $('.puTable').append("<tr class='rowRemove'><td class='rowRemove'>" + keyCount + " Systems Installed</td><td class='puCenter rowRemove'>" + countRanking + "</td></tr><tr class='rowRemove'><td class='rowRemove'>" + keyCap + " kW Capacity</td><td class='puCenter rowRemove'>" + capRanking + "</td></tr class='rowRemove'><tr><td class='rowRemove'>Avg. Cost: $" + keyCost + " / watt</td><td class='puCenter rowRemove'>" + costRanking + "</td></tr>");
-                    if (typeof stateData[key].policies == 'number') {
-                      $('.puTable').append("<tr class='rowRemove'><td class='rowRemove' colspan=2>" + stateData[key].policies + " Solar Policies Enacted</td></tr>")
-                    }
-                  }())
-                };
-              }
-            }
-          })
         }
-      })
-    }
+        function capRank () {
+          for (var i = 0; i < capArr.length; i++) {
+            if ( capArr[i][0] == data.name ) capRanking = i + 1;
+          }
+        }
+        function countRank () {
+          for (var i = 0; i < countArr.length; i++) {
+            if ( countArr[i][0] == data.name ) countRanking = i + 1;
+          }
+        }
+
+        // persistence function
+        if ( localStorage.getItem(stateName) !== 'null'){
+          clickCount = +(localStorage.getItem(stateName)) + 1;
+          localStorage.setItem(stateName, clickCount)
+        } else localStorage.setItem(stateName, clickCounter);
+
+        // pop-out function
+        $.ajax({
+          url: 'https://developer.nrel.gov/api/energy_incentives/v2/dsire.json?api_key=zRvnoStLNlMEuI4UIT0hyYzDa5j3p83JKfaVTbKs&address='+data.name+'&technology=solar_photovoltaics',
+          dataType: 'JSON',
+          type: 'GET',
+          success: function(response){
+            stateData[data.name].policies = response.result.length;
+          },
+        }).then(function(){
+          countRank(data.name);
+          capRank(data.name);
+          costRank(data.name);
+          for (var key in stateData) {
+            keyCount = stateData[key].count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            keyCap = stateData[key].cap.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            keyCost = stateData[key].cost.toFixed(2);
+
+            if ( clickVerifier === 1 || clickVerifier === 0 ) {
+              if(data.name == key){
+                (function pop (){
+                  $('.pu').show();
+                  $('.puTitleHolder').append("<div class='puTitle'><h3>" + stateName + "</h3></div>");
+                  $('.puTable').append("<tr class='rowRemove'><td class='rowRemove'>" + keyCount + " Systems Installed</td><td class='puCenter rowRemove'>" + countRanking + "</td></tr><tr class='rowRemove'><td class='rowRemove'>" + keyCap + " kW Capacity</td><td class='puCenter rowRemove'>" + capRanking + "</td></tr class='rowRemove'><tr><td class='rowRemove'>Avg. Cost: $" + keyCost + " / watt</td><td class='puCenter rowRemove'>" + costRanking + "</td></tr>");
+                  if (typeof stateData[key].policies == 'number') {
+                    $('.puTable').append("<tr class='rowRemove'><td class='rowRemove' colspan=2>" + stateData[key].policies + " Solar Policies Enacted</td></tr>")
+                  }
+                }())
+              };
+            }else{
+              clickVerifier = 0;
+            }
+          }
+        })
+      }
+    })
     // Why it matters
     $('#ddWhy').on('click', function(event) {
       $('#whyHidden').show();
